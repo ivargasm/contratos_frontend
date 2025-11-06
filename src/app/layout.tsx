3944cb108c309f0f9fbeda42aa7a3496f206f0b5
isvargasm <ismael.vargas@storecheck.com>
2025-11-06 12:12:21 -0600

    vercel analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner"
 import Navbar from "./components/navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
